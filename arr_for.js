b229aa163d779a285d39361e23d0f9b8653953d5
hyunjiq <edalnim12@gmail.com>
2024-05-29 12:21:17 +0900

    for문

--- arr_for.js
+++ arr_for.js
@@ -1,17 +1,26 @@
-const nav_data = [
-    {
-        gnb_li_a : "회사소개",
-        gnb_li_href : "./company.html"
-    },
-    {
-        gnb_li_a : "제품소개",
-        gnb_li_href : "./product.html"
-    },
-    {
-        gnb_li_a : "커뮤니티",
-        gnb_li_href : "./community.html"
-    },
+// const nav_data = [
+//     {
+//         gnb_li_a : "회사소개",
+//         gnb_li_href : "./company.html"
+//     },
+//     {
+//         gnb_li_a : "제품소개",
+//         gnb_li_href : "./product.html"
+//     },
+//     {
+//         gnb_li_a : "커뮤니티",
+//         gnb_li_href : "./community.html"
+//     },
     
-]
+// ]
 
-console.log(nav_data[2].gnb_li_a);
+// console.log(nav_data[2].gnb_li_a);
+
+
+console.log(`한번만 실행되지`);
+
+// 초기값, 조건, 증가값(거짓)
+// i는 0인데 i가 5까지만 i가 하나씩커질때
+for(var i = 0; i < 5; i++ ){
+    console.log(`나는 다섯번 실행되지`);
+}
