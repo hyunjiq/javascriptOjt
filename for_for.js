39c5c6fb91ffec3310eeec8d2e6fe3d7c40cf74e
hyunjiq <edalnim12@gmail.com>
2024-05-31 11:33:42 +0900

    dateSet

---
+++ for_for.js
@@ -0,0 +1,15 @@
+let dayoutput = "";
+
+for(let weeknm = 0; weeknm < 4; weeknm++){
+    
+    dayoutput +=`\n`;
+
+    for(let daynm = 1; daynm < 8; daynm++){
+
+            dayoutput += `${daynm}\t`;
+
+    }
+
+}
+
+console.log(dayoutput);
