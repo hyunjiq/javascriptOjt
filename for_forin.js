7148bdaa010fbee3ff6d49d6a9e9fb512d7b3e7d
hyunjiq <edalnim12@gmail.com>
2024-06-03 11:03:40 +0900

    for안에 for in 대메뉴 안에 소메뉴 넣는 것

---
+++ for_forin.js
@@ -0,0 +1,11 @@
+let conut = 0;
+for(){
+    conut += `<li>`;
+    conut += `<a href="">대메뉴</a>`
+    conut += `<ul>`
+    for(){
+        conut += `<li>소메뉴</li>`
+    }
+    conut +=`</ul>`
+    conut += `</li>`
+};
